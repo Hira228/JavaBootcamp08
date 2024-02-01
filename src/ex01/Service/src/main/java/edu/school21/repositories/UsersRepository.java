405dd02851f73f6be23c1f33e7a5db4274be6b15
Grady Zankale <gradyzan@at-g1.msk.21-school.ru>
2024-02-01 23:16:13 +0300

    ex01

--- src/ex01/Service/src/main/java/edu/school21/repositories/UsersRepository.java
+++ src/ex01/Service/src/main/java/edu/school21/repositories/UsersRepository.java
@@ -1,7 +1,8 @@
 package edu.school21.repositories;
 
+import java.sql.SQLException;
 import java.util.Optional;
 
 public interface UsersRepository<T> extends CrudRepository<T> {
-    Optional<T> findByEmail(String email);
+    Optional<T> findByEmail(String email) throws SQLException;
 }
